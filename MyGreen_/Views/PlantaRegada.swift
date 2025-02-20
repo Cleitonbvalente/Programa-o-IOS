8b90d2eb12be31a9bcd4610905e2a99063196ea9
Cleiton <cleiton4792bar@gmail.com>
2025-02-20 02:08:49 -0300

    Descrição das mudanças

---
+++ MyGreen_/Views/PlantaRegada.swift
@@ -0,0 +1,103 @@
+//
+//  PlantaRegada.swift
+//  MyGreen_
+//
+//  Created by iredefbmac_30 on 19/02/25.
+//
+
+import SwiftUI
+
+
+struct PlantaRegadaView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var planta: Planta
+    
+    var body: some View {
+        ZStack {
+            Image(planta.imagem)
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 20) {
+                VStack {
+                    Text(planta.nome)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text("(\(planta.nomeCienfitico))")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .italic()
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .background(Color("FontGreenDark"))
+                .cornerRadius(12)
+                .padding(.top, 40)
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack {
+                        Image(systemName: "face.smiling")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white)
+                        Text("OK")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .background(Color("FontGreenDark"))
+                    .cornerRadius(12)
+                    
+                    HStack {
+                        Image(systemName: "drop.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.white)
+                        Text("Próxima rega: 14 horas")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .background(Color("FontGreenDark"))
+                    .cornerRadius(12)
+                }
+                .padding(.leading, 200)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Planta Regada")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, minHeight: 50)
+                        .background(Color("FontGreenDark"))
+                        .cornerRadius(12)
+                        .padding()
+                }
+                .padding(.horizontal)
+            }
+        }
+        #if os(iOS)
+        .navigationBarHidden(true)
+        #endif
+    }
+}
+
+
+struct PlantaRegadaView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlantaRegadaView(planta: Planta(nome: "Zamioculta", nomeCienfitico: "Zamioculcas zamiifolia", descricao: "Descrição da planta", imagem: "Zamioculta", toxidade: "Leve", ervaDaninha: "Não", invasividade: "Não", tipoPlanta: "Ornamental", vidaUtil: "5-10 anos", tempoPlantio: "Primavera"))
+    }
+}
